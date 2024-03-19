cabfd1fb222c0522777a965f542c7535a61db683
tisacra <tisacra.i.c@gmail.com>
2024-03-20 02:08:11 +0900

    type-A servo actions

---
+++ Control/stss_motor/test_mySTS.py
@@ -0,0 +1,12 @@
+import mySTS,time
+
+mySTS.DEVICENAME = '/dev/ttyUSB0'
+mySTS.prepare_STS()
+
+servo = mySTS.mySTS(1,1000000)
+servo.wheelmode(1)
+#servo.move_absolute_pos(100,1000,0) #0.acceralation 1.speed 2.position
+servo.move_speed(100,4096) #0.acceralation 1.speed
+time.sleep(1)
+servo.move_speed(50,0) #0.acceralation 1.speed
+mySTS.close()
