e1d20175d6cf9dfd8751f6b41584e94d2d039386
tisacra <tisacra.i.c@gmail.com>
2024-03-23 22:02:54 +0900

    rename folder

---
+++ Control/src/core.py
@@ -0,0 +1,84 @@
+from stss_nfc.stss_nfc import *
+import stss_gui
+import tkinter
+
+func_mode_debug = 1
+NFC_arg_debug = 'nishiyama'
+
+#main function : 0
+def main_menu(root):
+    global current_page
+    if stss_gui.after_id != None:
+        root.after_cancel(stss_gui.after_id)
+
+    nfc_detected = nfc.connect()
+    #func_mode = function_number
+    if nfc_detected:
+        #degugging
+        func_mode = int(input('enter function_number'))
+        #arg = argument
+        arg = NFC_arg_debug
+
+        current_page = func_mode
+        stss_gui.main_function[func_mode][1](root,0,arg) # start each function from phase : 0
+    else:
+        stss_gui.after_id = root.after(100,main_menu,root)
+
+stss_gui.main_function.append(('main_menu',main_menu))
+
+#main function : 1
+def deposit(root,phase,USER_NAME,cell_num = None):
+    global current_page
+    print('deposit : phase ' + str(phase))
+    if phase == 0:
+        stss_gui.Draw_Page(root,1)
+        stss_gui.Arrange_ToolButton(root)
+        stss_gui.wait_push(root,'tool_select',deposit,phase,USER_NAME)
+
+    elif phase == 1:
+        print('selected : ' + str(cell_num))
+
+        #CLI更新
+
+        #ボタン非表示
+
+        #回転
+
+        #ドア開く
+
+        #待機
+
+        #管理ファイル更新
+
+        #finish
+        current_page = 0
+
+stss_gui.flags['tool_select'] = (False,None)
+stss_gui.main_function.append(('deposit',deposit))
+
+#main function : 2
+def withdraw(SERIAL_NUMBER):
+    #実装
+    #
+    #
+    pass
+
+stss_gui.main_function.append(('withdraw',withdraw))
+
+
+
+#machine setting
+nfc = nfcReader()
+
+root = tkinter.Tk()
+
+#
+#
+#
+
+stss_gui.Draw_Page(root,0)
+
+root.after(100,main_menu,root)
+
+print('start')
+root.mainloop()
---
+++ Control/src/stss_motor/mySTS.py
@@ -0,0 +1,141 @@
+from scservo_sdk import *                # Uses SCServo SDK library
+# Control table address
+ADDR_SCS_TORQUE_ENABLE     = 40
+ADDR_SCS_GOAL_ACC          = 41
+ADDR_SCS_GOAL_POSITION     = 42
+ADDR_SCS_GOAL_SPEED        = 46
+ADDR_SCS_PRESENT_POSITION  = 56
+
+ADDR_SCS_MODE = 33
+
+import os
+import sys, tty, termios
+
+if os.name == 'nt':
+        import msvcrt
+        def getch():
+            return msvcrt.getch().decode()
+else:
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        def getch():
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+        
+DEVICENAME = ""    # define in your code
+portHandler = None
+packetHandler = None
+
+def prepare_STS():
+    global portHandler,packetHandler
+    protocol_end = 0                 # SCServo bit end(STS/SMS=0, SCS=1)
+    
+    # Initialize PortHandler instance
+    # Set the port path
+    # Get methods and members of PortHandlerLinux or PortHandlerWindows
+    portHandler = PortHandler(DEVICENAME)
+
+    # Initialize PacketHandler instance
+    # Get methods and members of Protocol
+    packetHandler = PacketHandler(protocol_end)
+
+    # Open port
+    if portHandler.openPort():
+        print("Succeeded to open the port")
+    else:
+        print("Failed to open the port")
+        print("Press any key to terminate...")
+        getch()
+        quit()
+
+class mySTS:
+    global portHandler,packetHandler
+    def __init__(self,sts_id,baudrate):
+        self.SCS_ID = sts_id
+        self.Baudrate = baudrate
+
+        # Open port
+        if portHandler.openPort():
+            print("Succeeded to open the port")
+        else:
+            print("Failed to open the port")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+        # Set port baudrate
+        if portHandler.setBaudRate(self.Baudrate):
+            print("Succeeded to change the baudrate")
+        else:
+            print("Failed to change the baudrate")
+            print("Press any key to terminate...")
+            getch()
+            quit()
+
+        # Try to ping the SCServo
+        # Get SCServo model number
+        scs_model_number, scs_comm_result, scs_error = packetHandler.ping(portHandler, self.SCS_ID)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+        else:
+            print("[ID:%03d] ping Succeeded. SCServo model number : %d" % (self.SCS_ID, scs_model_number))
+    
+    def wheelmode(self,mode):
+        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_MODE, mode)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+    def move_absolute_pos(self,acc,speed,position):
+        # Write SCServo acc
+        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_GOAL_ACC, acc)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+        # Write SCServo speed
+        scs_comm_result, scs_error = packetHandler.write2ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_GOAL_SPEED, speed)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+        # Write SCServo goal position
+        scs_comm_result, scs_error = packetHandler.write2ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_GOAL_POSITION, position)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+    
+    def move_speed(self,acc,speed):
+        # Write SCServo acc
+        scs_comm_result, scs_error = packetHandler.write1ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_GOAL_ACC, acc)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+        _speed_ = speed
+        # Write SCServo speed
+        if speed<0 :
+            _speed_ = -speed
+            _speed_ |= (1<<15)
+	    
+        scs_comm_result, scs_error = packetHandler.write2ByteTxRx(portHandler, self.SCS_ID, ADDR_SCS_GOAL_SPEED, _speed_)
+        if scs_comm_result != COMM_SUCCESS:
+            print("%s" % packetHandler.getTxRxResult(scs_comm_result))
+        elif scs_error != 0:
+            print("%s" % packetHandler.getRxPacketError(scs_error))
+
+def close():
+    global portHandler,packetHandler
+    # Close port
+    portHandler.closePort()
